a324402c23ad046e5f9eaab5bea95630cf466b42
Daseul Kim <kimdaseel@naver.com>
2026-07-24 12:27:48 +0900

    Add videoList array with video details

---
+++ video.js
@@ -0,0 +1,15 @@
+const videoList = [
+    {
+        tag: "🎥 하이라이트",
+        title: "원하는 영상 제목 1",
+        desc: "영상에 대한 간단한 설명이나 경기 날짜를 입력하세요.",
+        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    },
+    {
+        tag: "📺 인터뷰",
+        title: "원하는 영상 제목 2",
+        desc: "유튜브 주소 전체를 그대로 넣으셔도 자동으로 인식됩니다!",
+        url: "https://youtu.be/dQw4w9WgXcQ"
+    }
+    // 💡 새로운 영상을 추가하려면 아래처럼 컴마(,)를 찍고 줄을 추가하시면 됩니다!
+];
