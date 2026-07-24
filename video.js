2da34c6277c8e1a6c82dbfd63d8f36103a4341a9
Daseul Kim <kimdaseel@naver.com>
2026-07-24 12:33:48 +0900

    Update video list with new entries and details

--- video.js
+++ video.js
@@ -1,15 +1,15 @@
 const videoList = [
     {
-        tag: "🎥 하이라이트",
-        title: "원하는 영상 제목 1",
-        desc: "영상에 대한 간단한 설명이나 경기 날짜를 입력하세요.",
-        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        tag: "🎥 신한은행 유튜브",
+        title: "에스버드 더비 성사(?) | 3X3 트리플잼 비하인드 📁",
+        desc: "유정 선수와 현정 선수가 출전한 3x3 트리플잼 현장을 에스버드TV가 담아왔습니다.📺",
+        url: "https://youtu.be/6W-8yj9lgmE?si=6puWvSYBfjc7P4vN"
     },
     {
         tag: "📺 인터뷰",
-        title: "원하는 영상 제목 2",
-        desc: "유튜브 주소 전체를 그대로 넣으셔도 자동으로 인식됩니다!",
-        url: "https://youtu.be/dQw4w9WgXcQ"
+        title: "'3점 대폭발' 팀내 최다 득점 기록한 허유정 인터뷰 [2025 WKBL 퓨처스리그]",
+        desc: "2025 WKBL 퓨처스리그 허유정 인터",
+        url: "https://naver.me/xpBbXci4"
     }
     // 💡 새로운 영상을 추가하려면 아래처럼 컴마(,)를 찍고 줄을 추가하시면 됩니다!
 ];
