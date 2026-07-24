c6b41b30b65e075b923051ab900a993a78505f23
Daseul Kim <kimdaseel@naver.com>
2026-07-24 16:19:01 +0900

    Update and rename video.js to videos.js

--- videos.js
+++ videos.js
@@ -3,7 +3,7 @@ const videoList = [
         tag: "🎥 신한은행 유튜브",
         title: "에스버드 더비 성사(?) | 3X3 트리플잼 비하인드 📁",
         desc: "유정 선수와 현정 선수가 출전한 3x3 트리플잼 현장을 에스버드TV가 담아왔습니다.📺",
-        url: "https://youtu.be/6W-8yj9lgmE?si=6puWvSYBfjc7P4vN" // 👈 유튜브는 복사한 주소 그대로 사용하시면 됩니다!
+        url: "https://youtu.be/6W-8yj9lgmE?si=6puWvSYBfjc7P4vN" // 
     },
     {
         tag: "📺 네이버 티비",
